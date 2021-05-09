3bc18d71f90de96d8722be33bf4c03b43ee53cf0
ianmateusES <ec.ianmateus@alu.ufc.br>
2021-05-09 20:46:39 -0300

    feat: New architecture

--- src/modules/categories/repositories/ICategoriesRepository.ts
+++
@@ -1,8 +0,0 @@
-import ICreateCategoryDTO from '../dtos/ICreateCategoryDTO';
-import Category from '../model/Category';
-
-export default interface ICategoriesRepository {
-  create(data: ICreateCategoryDTO): Category;
-  findAll(): Category[];
-  findByName(name: string): Category | undefined;
-}
