662161d6044bfed046691255e8e080dc81af33fd
ianmateusES <ec.ianmateus@alu.ufc.br>
2021-05-13 02:45:09 -0300

    feat: Create user avatar

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { UpdateUserAvatarUseCase } from './UpdateUserAvatarUseCase';
+
+class UpdateUserAvatarController {
+  public async handle(req: Request, res: Response): Promise<Response> {
+    const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
+
+    const user = await updateUserAvatarUseCase.execute({
+      user_id: req.user.id,
+      avatarFilename: req.file.filename,
+    });
+
+    return res.status(201).json(user);
+  }
+}
+
+export { UpdateUserAvatarController };
